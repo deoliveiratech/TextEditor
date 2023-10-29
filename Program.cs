14a1b6e2283362ab3868a4e342d0487e2020b6a8
Rodrigo R. de Oliveira <digoliveira.tec@gmail.com>
2023-10-28 23:09:15 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1 - Criar novo arquivo");
+            Console.WriteLine("2 - Abrir arquivo");
+            Console.WriteLine("0 - Sair");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: Sair(); break;
+                case 1: Criar(); break;
+                case 2: Abrir(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Sair()
+        {
+            Console.WriteLine("------");
+            Console.WriteLine("LOGOFF");
+            Console.WriteLine("------");
+            Environment.Exit(0);
+        }
+
+        static void Abrir()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho do arquivo?");
+            string path = Console.ReadLine();
+            Console.WriteLine("------------------------");
+
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
+        }
+
+        static void Criar()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo: (ENTER + ESC para sair e salvar)");
+            Console.WriteLine("----------------");
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Salvar(text);
+        }
+
+        static void Salvar(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho para salvar o arquivo?");
+            var path = Console.ReadLine();
+
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+
+            Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+            Console.ReadLine();
+            Menu();
+        }
+    }
+}
